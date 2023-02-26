5dc9c8458c3eafb36cf95ea65b75fd48f361423b
Alexander Guzman Rodriguez <alex_guzmanrodriguez_7@outlook.com>
2023-02-25 21:46:37 -0500

    Initial commit, Create project

---
+++ src/main/java/com/alexguzrod/Springreactivegeolocation/SpringReactiveGeolocationApplication.java
@@ -0,0 +1,13 @@
+package com.alexguzrod.Springreactivegeolocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveGeolocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveGeolocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexguzrod/Springreactivegeolocation/SpringReactiveGeolocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexguzrod.Springreactivegeolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveGeolocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
